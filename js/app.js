39b3e0c75533eecdb78f45b79ff8491cb7ff5aea
Ava Melchior <avacmelchior@gmail.com>
2023-08-31 20:33:15 -0500

    edits

---
+++ js/app.js
@@ -0,0 +1,9 @@
+'use strict';
+console.log('app js is running!');
+
+let userName = prompt('What is your name, dear?');
+console.log('username', userName);
+
+let message;
+message = 'We have been waiting,'
+document.write(message + ' ' + userName + '!');
